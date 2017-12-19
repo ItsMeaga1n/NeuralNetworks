ec53bcfa6d9c0ba1c0595adace7f6dd280c25843
ItsMeaga1n <peinpat1@gmail.com>
2017-12-19 01:34:25 +0100

    fix?

--- zad_3/perceptron.js
+++ zad_3/perceptron.js
@@ -10,8 +10,15 @@ class Perceptron {
       this.pocket = null;
     }
 
-    adjustWeights(){
-        
+    getPrzyklad(nrPrzykladu){
+        let fftReal = [...numbers[nrPrzykladu]];
+        let fftImag = new Array(36+1).join('0').split('').map(parseFloat);
+        transform(fftReal, fftImag)
+        let fftPrzyklad = new Array;
+        for(let i = 0; i < 36; i++){
+             fftPrzyklad.push(Math.pow(Math.pow(fftReal[i],2) + Math.pow(fftImag[i], 2),0.5));
+        }
+        return [...numbers[nrPrzykladu], ...fftPrzyklad];
     }
 
     learn(){
@@ -37,45 +44,36 @@ class Perceptron {
         // }
         let rand = Math.random()
         var examples = [0,1,2,3,4,5,6,7,8,9];
-        for(let i = 5; i < 1000; i++){
+        for(let i = 5; i < 100; i++){
             shuffle(examples);
            let nrPrzykladu = examples[i%10];
-           let przyklad = numbers[nrPrzykladu];
-           let fft = [...przyklad];
-
+           let przyklad = this.getPrzyklad(nrPrzykladu);
            let wynik = 0;
            if(nrPrzykladu === this.number){
                wynik = 1;
            }
-           let imag = new Array(36+1).join('0').split('').map(parseFloat);
-           transform(fft, imag)
-           let fftPrzyklad = new Array;
-           for(let i = 0; i < 36; i++){
-                fftPrzyklad.push(Math.pow(Math.pow(fft[i],2) + Math.pow(imag[i], 2),0.5));
-           }
-           przyklad = [...przyklad, ...fftPrzyklad];
            let suma = this.count(przyklad);
            for(let j = 0; j < przyklad.length; j++){
                this.weights[j+1] = this.weights[j+1] + this.learningRate * (wynik - suma);
            }
            this.weights[0] = this.weights[0] + this.learningRate * (wynik - suma);
-           if(i % 5 === 0){
-               let err = 0;
-               let spodziewanyWynik;
-               for(let j = 0; j < 10; j++){
-                   if(this.number === j) {
-                       spodziewanyWynik = 1;
-                   }
-                   else {
-                    spodziewanyWynik = 0;
-                   }
-                   let otrzymanyWynik = this.count(numbers[j]);
-                   err += (otrzymanyWynik - spodziewanyWynik) * (otrzymanyWynik - spodziewanyWynik);
-               }
-               if(err < 0.0001){
-                   break;
-               }
-           }
+            if(i % 5 === 0){
+                let err = 0;
+                let spodziewanyWynik;
+                for(let j = 0; j < 10; j++){
+                    if(this.number === j) {
+                        spodziewanyWynik = 1;
+                    }
+                    else {
+                     spodziewanyWynik = 0;
+                    }
+                    let otrzymanyWynik = this.count(this.getPrzyklad(j));
+                    err += (otrzymanyWynik - spodziewanyWynik) * (otrzymanyWynik - spodziewanyWynik);
+                }
+                if(err < 0.0001){
+                    break;
+                }
+            }
         }
     }
 
@@ -114,9 +112,9 @@ class Perceptron {
             sum += this.weights[i+1] * x[i];
         }
         sum += this.weights[0];
-        console.log('liczba: '+this.number, 1/(1 + Math.exp(-sum)));
-        
-        return 1/(1 + Math.exp(-sum));
+        let result = 1/(1 + Math.exp(-sum));
+        console.log('adeline '+this.number, sum, x, result)
+        return result;
     }
 
     signum(val){
@@ -137,7 +135,8 @@ class Perceptron {
         for(let i = 0; i < 36; i++){
              fftPrzyklad.push(Math.pow(Math.pow(tempGrid[i],2) + Math.pow(imag[i], 2),0.5));
         }
-        return this.count(fftPrzyklad);
+        console.log(this.count([...grid, ...fftPrzyklad]))
+        return this.count([...grid, ...fftPrzyklad]);
     }
 }
 
